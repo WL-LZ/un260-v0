0469824d63623ea39977d2f567c3d627a7033454
xl <2437627483@qq.com>
2026-03-26 17:14:14 +0800

    feat: 添加加载控价

---
+++ un260/lv_components/lv_loading_orbit.c
@@ -0,0 +1,85 @@
+#include "lv_loading_orbit.h"
+
+#define LOADING_ORBIT_SIZE_DEFAULT      38
+#define LOADING_ORBIT_RING_RATIO_NUM    24
+#define LOADING_ORBIT_RING_RATIO_DEN    38
+#define LOADING_ORBIT_ARC_WIDTH_DEFAULT 3
+#define LOADING_ORBIT_ROTATE_TIME       900
+
+static lv_coord_t loading_orbit_clamp(lv_coord_t v, lv_coord_t min_v)
+{
+    return v < min_v ? min_v : v;
+}
+
+static void loading_orbit_rotate_cb(void *var, int32_t v)
+{
+    lv_arc_set_rotation((lv_obj_t *)var, (int16_t)v);
+}
+
+lv_obj_t *lv_loading_orbit_create_sized(lv_obj_t *parent, lv_coord_t size)
+{
+    lv_coord_t orbit_size = loading_orbit_clamp(size, 20);
+    lv_coord_t ring_size = loading_orbit_clamp((orbit_size * LOADING_ORBIT_RING_RATIO_NUM) /
+                                               LOADING_ORBIT_RING_RATIO_DEN, 12);
+    lv_coord_t arc_width = loading_orbit_clamp((orbit_size * LOADING_ORBIT_ARC_WIDTH_DEFAULT) /
+                                               LOADING_ORBIT_SIZE_DEFAULT, 2);
+    lv_coord_t accent_size = loading_orbit_clamp(orbit_size / 5, 4);
+    lv_coord_t accent_ofs = loading_orbit_clamp(orbit_size / 10, 3);
+
+    lv_obj_t *host = lv_obj_create(parent);
+    lv_obj_remove_style_all(host);
+    lv_obj_set_size(host, orbit_size, orbit_size);
+    lv_obj_clear_flag(host, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_clear_flag(host, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_set_style_radius(host, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_style_bg_color(host, lv_color_hex(0xF5F8FC), 0);
+    lv_obj_set_style_bg_opa(host, 220, 0);
+    lv_obj_set_style_border_width(host, 0, 0);
+    lv_obj_set_style_shadow_color(host, lv_color_hex(0x000000), 0);
+    lv_obj_set_style_shadow_opa(host, LV_OPA_10, 0);
+    lv_obj_set_style_shadow_width(host, 8, 0);
+    lv_obj_set_style_shadow_ofs_x(host, 1, 0);
+    lv_obj_set_style_shadow_ofs_y(host, 2, 0);
+    lv_obj_set_style_pad_all(host, 0, 0);
+
+    lv_obj_t *arc = lv_arc_create(host);
+    lv_obj_set_size(arc, ring_size, ring_size);
+    lv_obj_center(arc);
+    lv_obj_remove_style(arc, NULL, LV_PART_KNOB);
+    lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE);
+    lv_arc_set_range(arc, 0, 100);
+    lv_arc_set_bg_angles(arc, 0, 360);
+    lv_arc_set_rotation(arc, 270);
+    lv_arc_set_value(arc, 22);
+    lv_obj_set_style_arc_width(arc, arc_width, LV_PART_MAIN);
+    lv_obj_set_style_arc_width(arc, arc_width, LV_PART_INDICATOR);
+    lv_obj_set_style_arc_color(arc, lv_color_hex(0xD7E1EB), LV_PART_MAIN);
+    lv_obj_set_style_arc_opa(arc, LV_OPA_70, LV_PART_MAIN);
+    lv_obj_set_style_arc_color(arc, lv_color_hex(0x2F7CF6), LV_PART_INDICATOR);
+    lv_obj_set_style_arc_rounded(arc, true, LV_PART_INDICATOR);
+
+    lv_obj_t *accent = lv_obj_create(host);
+    lv_obj_remove_style_all(accent);
+    lv_obj_set_size(accent, accent_size, accent_size);
+    lv_obj_set_style_radius(accent, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_style_bg_color(accent, lv_color_hex(0xC7EB00), 0);
+    lv_obj_set_style_bg_opa(accent, LV_OPA_COVER, 0);
+    lv_obj_align(accent, LV_ALIGN_TOP_RIGHT, -accent_ofs, accent_ofs);
+
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_var(&a, arc);
+    lv_anim_set_exec_cb(&a, loading_orbit_rotate_cb);
+    lv_anim_set_values(&a, 0, 360);
+    lv_anim_set_time(&a, LOADING_ORBIT_ROTATE_TIME);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_set_path_cb(&a, lv_anim_path_linear);
+    lv_anim_start(&a);
+
+    return host;
+}
+
+lv_obj_t *lv_loading_orbit_create(lv_obj_t *parent)
+{
+    return lv_loading_orbit_create_sized(parent, LOADING_ORBIT_SIZE_DEFAULT);
+}
---
+++ un260/lv_components/lv_loading_orbit.h
@@ -0,0 +1,9 @@
+#ifndef LV_LOADING_ORBIT_H
+#define LV_LOADING_ORBIT_H
+
+#include "lvgl/lvgl.h"
+
+lv_obj_t *lv_loading_orbit_create(lv_obj_t *parent);
+lv_obj_t *lv_loading_orbit_create_sized(lv_obj_t *parent, lv_coord_t size);
+
+#endif
